dcccf32e776e9825a54293c3c7a5407d50051851
Fayorr <fayokunmiosho@gmail.com>
2022-09-29 13:57:51 +0100

    navbar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import { Route , Routes, Navigate } from 'react-router-dom';
+// import './App.css';
+// import Home from "./components/Home"
+// import  About  from "./components/About"
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    
+   <Navbar />
+      // <Routes>
+      //   <Route path="/" element={} />
+      //   <Route path="/about" element={<About />} />
+      //   <Route path="/hello" element={<Navigate to="/home" />} />
+      // </Routes>
+    
+  );
+}
+
+export default App;
